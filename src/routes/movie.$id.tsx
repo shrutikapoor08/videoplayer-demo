e0d1c1187fe9df528e6be4de2dd9c88626948434
shrutikapoor08 <shruti.kapoor08@gmail.com>
2025-10-22 13:25:51 -0400

    Adding readme

--- src/routes/movie.$id.tsx
+++ src/routes/movie.$id.tsx
@@ -1,7 +1,13 @@
-import MovieDetail from '@/components/MovieDetail'
-// import { lazy } from 'react'
-// const MovieDetail = lazy(() => import('@/components/MovieDetail'))
+import { lazy, Suspense } from 'react'
 import { createFileRoute } from '@tanstack/react-router'
+import MovieDetail from '@/components/MovieDetail'
+// const LazyMovieDetail = lazy(() => import('@/components/MovieDetail'))
+
+// const MovieDetailWrapper = (props: any) => (
+//   // <Suspense fallback={<div>Loading movie…</div>}>
+//     // <LazyMovieDetail {...props} />
+//   // </Suspense>
+// )
 
 export const Route = createFileRoute('/movie/$id')({
   component: MovieDetail,
